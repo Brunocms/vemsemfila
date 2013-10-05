62bd969bc78f70369e031af7919f6823a53f79de
caioreis <morettieq@gmail.com>
2013-10-05 15:18:48 -0300

    Alterando arquviocs

---
+++ application/controllers/admin.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+    public $meta;
+    public $data;
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('m_usuario');
+        $this->load->library(['user/user', 'user/user_manager']);
+        $this->user->on_invalid_session(base_url('home'));
+        if (!$this->user_manager->user_permission($this->user->get_id(), 9))
+            redirect(base_url('home'));
+    }
+
+	public function index()
+	{
+        $this->meta['header'] = $this->load->view('header', '', true);
+		$this->meta['footer'] = $this->load->view('footer', '', true);
+        $this->meta['topo'] = $this->load->view('topo' , '', true);
+        $this->load->view('admin/cadastro_rest', $this->meta);
+	}
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/admin/cadastro_rest.php
@@ -0,0 +1,79 @@
+<?=$header?>
+<?=$topo?>
+<div id="content">
+<div id="middle">
+<h1>Cadastro de Restaurante:</h1><img src="img/linha_tit_home.gif" width="734" height="1" />
+<div id="txt">Preenchar o formulário abaixo para se cadastrar e ter acesso ao nosso serviço tanto no seu computador quanto no seu smartphone.<br /><br />O Número de Celular é necessário para podermos enviar notificações do seu check-in na fila por sms.<br /><br />
+  <table width="651" border="0" cellspacing="3" cellpadding="8">
+    <tr>
+      <td height="24" colspan="4" bgcolor="#CECECE" class="table"><strong>INFORMAÇÕES:</strong></td>
+      </tr>
+    <tr>
+      <td width="102" bgcolor="#DFDFDF" class="table">*Nome do Rest.</td>
+      <td width="184" bgcolor="#DFDFDF" class="table"><label for="textfield"></label>
+        <input type="text" name="textfield" id="textfield" class="field" /></td>
+      <td width="100" bgcolor="#DFDFDF" class="table">*CNPJ</td>
+      <td width="186" bgcolor="#DFDFDF" class="table"><input type="text" name="textfield2" id="textfield2" class="field" /></td>
+    </tr>
+    <tr>
+      <td bgcolor="#DFDFDF" class="table">*Razão Social</td>
+      <td bgcolor="#DFDFDF" class="table"><input type="text" name="textfield3" id="textfield3" class="field" /></td>
+      <td bgcolor="#DFDFDF" class="table">*Enderço</td>
+      <td bgcolor="#DFDFDF" class="table"><input type="text" name="textfield4" id="textfield4" class="field" /></td>
+    </tr>
+    <tr>
+      <td bgcolor="#DFDFDF" class="table">*Nome do Contato</td>
+      <td bgcolor="#DFDFDF" class="table"><input type="text" name="textfield11" id="textfield11" class="field" /></td>
+      <td bgcolor="#DFDFDF" class="table">*E-mail</td>
+      <td bgcolor="#DFDFDF" class="table"><input type="text" name="textfield11" id="textfield12" class="field" /></td>
+    </tr>
+    <tr>
+      <td bgcolor="#DFDFDF" class="table">*Tel 1</td>
+      <td bgcolor="#DFDFDF" class="table"><input type="text" name="textfield11" id="textfield17" class="field" /></td>
+      <td bgcolor="#DFDFDF" class="table">Tel 2</td>
+      <td bgcolor="#DFDFDF" class="table"><input type="text" name="textfield11" id="textfield18" class="field" /></td>
+    </tr>
+    <tr>
+      <td colspan="2" bgcolor="#DFDFDF" class="table">*Categoria: 
+        <input type="radio" name="radio5" id="radio8" value="radio5" />
+        <label for="radio8"></label>
+        Matriz
+        <input type="radio" name="radio5" id="radio9" value="radio5" />
+        <label for="radio9"></label>
+        Filial
+        <input type="radio" name="radio5" id="radio10" value="radio5" />
+        <label for="radio10"></label>
+        Franquia </td>
+      <td colspan="2" bgcolor="#DFDFDF" class="table">Cadastrar Filial agora?
+        <input type="radio" name="radio5" id="radio" value="radio5" />
+Sim
+<input type="radio" name="radio5" id="radio2" value="radio5" />
+Não</td>
+      </tr>
+    <tr>
+      <td bgcolor="#DFDFDF" class="table">*Senha</td>
+      <td bgcolor="#DFDFDF" class="table"><input type="text" name="textfield5" id="textfield5" class="field" /></td>
+      <td bgcolor="#DFDFDF" class="table">*Confirmar senha</td>
+      <td bgcolor="#DFDFDF" class="table"><input type="text" name="textfield5" id="textfield6" class="field" /></td>
+    </tr>
+    <tr>
+      <td colspan="4" bgcolor="#DFDFDF" class="table">Observação<br />
+        <label for="textarea"></label>
+        <textarea name="textarea" id="textarea" class="field2"></textarea></td>
+    </tr>
+    <tr>
+      <td colspan="4" align="right" class="table"><input type="submit" name="enviar" id="enviar" value="Enviar" class="send" /></td>
+    </tr>
+    </table>
+</div>
+<div id="divider"><img src="img/divider.png" width="734" height="1" /></div>
+<div id="destaques"><a href="#"><img src="img/box_downloadapp_home.jpg" width="237" height="109" alt="download app" /></a> <a href="cadastro_rest.html"><img src="img/box_cadastrorest_home.jpg" width="237" height="109" alt="cadastro restaurante" hspace="5" /></a> <a href="reserva.html"><img src="img/box_reserva_home.jpg" width="237" height="109" alt="reserva mesa" hspace="5" /></a></div>
+</div>
+
+<div id="publicidade">
+  <a href="#"><img src="img/pub01.jpg" width="173" height="292" alt="pub01" /></a><br />
+  <a href="#"><img src="img/pub02.jpg" width="173" height="260" alt="pub02" vspace="5" /></a><br />
+</div>
+
+</div>
+<?=$footer?>
