5188d9d78c8e9ea110b05a20b982395367d3fe93
caioreis <morettieq@gmail.com>
2013-10-07 23:07:51 -0300

    Arrumando FAQ

--- application/views/home/faq.php
+++ application/views/home/faq.php
@@ -1,19 +1,26 @@
 <?=$header?>
 <?=$topo?>
-<div id="content">
-<div id="middle">
-<h1>Perguntas Freqüentes:</h1><img src="img/linha_tit_home.gif" width="734" height="1" />
-<div id="txt">
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br /><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br /><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-</div>
-<div id="divider"><img src="img/divider.png" width="734" height="1" /></div>
-<div id="destaques"><a href="#"><img src="img/box_downloadapp_home.jpg" width="237" height="109" alt="download app" /></a> <a href="cadastro_rest.html"><img src="img/box_cadastrorest_home.jpg" width="237" height="109" alt="cadastro restaurante" hspace="5" /></a> <a href="reserva.html"><img src="img/box_reserva_home.jpg" width="237" height="109" alt="reserva mesa" hspace="5" /></a></div>
+<div class="container_24">
+    <div class="grid_24">
+
+        <div class="grid_19 alpha">
+            <h1>Perguntas frequentes</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br /><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br /><br />Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <div id="download_app"><a href="#"><img src="/assets/img/banner_app_home.png" width="737" height="77" alt="baixe aqui nosso app!" title="baixe aqui nosso app!" /></a></div>
+        </div>
+
+        <div class="grid_4 prefix_1 omega">
+            <a href="#"><img src="/assets/img/pub01.jpg" width="173" height="292" alt="pub01" /></a><br />
+            <a href="#"><img src="/assets/img/pub02.jpg" width="173" height="260" alt="pub02" vspace="5" /></a><br />
+            <a href="#"><img src="/assets/img/pub03.jpg" width="173" height="171" alt="pub03" vspace="5" /></a><br />
+        </div>
+
+    </div>
+
+
 </div>
 
-<div id="publicidade">
-  <a href="#"><img src="img/pub01.jpg" width="173" height="292" alt="pub01" /></a><br />
-  <a href="#"><img src="img/pub02.jpg" width="173" height="260" alt="pub02" vspace="5" /></a><br />
-</div>
-
-</div>
 <?=$footer?>
