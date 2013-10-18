630d6e8d94940f3d08702ae149ae8d03c0da30ed
frisone <frisonemx@gmail.com>
2013-10-17 21:08:17 -0300

    pagina de restaurantes com listagem tabular

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -3,13 +3,18 @@
 <div class="container_24">
     <div class="grid_24">
 
-        <div class="grid_20 alpha">
+        <div class="grid_19 alpha">
+            
             <h1><img src="/assets/img/icon_relogio_tit.jpg" width="17" height="17" align="absmiddle" /> Tempo de espera agora:</h1>
+            
             <div id="alerta">*Tempo de espera na fila baseado em mesa para 4 pessoas</div>
+            
             <div id="gestao">
+
                 <div id="busca"><strong>Escreva o nome ou o bairro do resturante aqui: </strong><input name="" type="text" class="field3" /><a href="#">VEM</a> </div>
 
                 <span id="nome">Restaurante</span><span id="tempo">Tempo de fila</span><span id="mesas">N° de Mesas</span><span id="endereco">Endereço</span>
+
                 <ul>
                     <li><span class="thumb"><img src="/assets/img/thumb_outback.png" width="62" height="31" alt="outback" /></span><span class="nome">Outback Rio Sul</span><span class="tempo">0 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
                     <li><span class="thumb"><img src="/assets/img/thumb_gulagula.png" width="62" height="31" alt="gula gula" /></span><span class="nome">Gula Gula</span><span class="tempo">20 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
@@ -17,13 +22,16 @@
                     <li><span class="thumb"><img src="/assets/img/thumb_joeleos.png" width="62" height="31" alt="joeleos" /></span><span class="nome">Joe & Leo's</span><span class="tempo">45 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
                     <li><span class="thumb"><img src="/assets/img/thumb_porcao.png" width="62" height="31" alt="porcão" /></span><span class="nome">Porcão Rio's</span><span class="tempo">55 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
                 </ul>
+
             </div>
-            <div id="divider"><img src="/assets/img/divider.png" width="734" height="1" /></div>
+            
             <div id="destaques"><a href="cadastro_lead.html"><img src="/assets/img/banner_home.jpg" width="737" height="200" alt="vem!" title="vem!" /></a></div>
+            
             <div id="download_app"><a href="#"><img src="/assets/img/banner_app_home.png" width="737" height="77" alt="baixe aqui nosso app!" title="baixe aqui nosso app!" /></a></div>
+
         </div>
     
-        <div class="grid_4 omega">
+        <div class="grid_4 prefix_1 omega">
             <a href="#"><img src="/assets/img/pub01.jpg" width="173" height="292" alt="pub01" /></a><br />
             <a href="#"><img src="/assets/img/pub02.jpg" width="173" height="260" alt="pub02" vspace="5" /></a><br />
             <a href="#"><img src="/assets/img/pub03.jpg" width="173" height="171" alt="pub03" vspace="5" /></a><br />
--- application/views/restaurantes/index.php
+++ application/views/restaurantes/index.php
@@ -1,45 +1,78 @@
 <?=$header?>
 <?=$topo?>
-<div id="content">
-<div id="middle">
-<h1>Restaurantes:</h1><img src="img/linha_tit_home.gif" width="734" height="1" />
-<div id="alerta">*Tempo de espera na fila baseado em mesa para 4 pessoas</div>
-<div id="gestao">
-<div id="busca"><strong>Buscar por:</strong> &nbsp;&nbsp;<select name="" class="field4"><option>Bairro</option></select> &nbsp;&nbsp;&nbsp;<select name="" class="field5"><option>Shopping Rio Sul</option></select> &nbsp;&nbsp;&nbsp;Ou escreva um nome aqui: <input name="" type="text" class="field3" /><a href="#"><img src="img/bt_vem.png" width="35" height="20" alt="vem" align="absmiddle" style="margin-left:-36px; margin-top:-2px" /></a> </div>
- 
- <span id="nome">Restaurante</span><span id="tempo">Tempo de fila</span><span id="mesas">N° de Mesas</span><span id="endereco">Endereço</span>
-<ul>
-<li><span class="thumb"><img src="/assets/img/thumb_outback.png" width="62" height="31" alt="outback" /></span><span class="nome">Outback Rio Sul</span><span class="tempo">0 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="/assets/img/thumb_gulagula.png" width="62" height="31" alt="gula gula" /></span><span class="nome">Gula Gula</span><span class="tempo">20 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="/assets/img/thumb_braz.png" width="62" height="31" alt="braz" /></span><span class="nome">Pizzaria Bráz</span><span class="tempo">40 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="/assets/img/thumb_joeleos.png" width="62" height="31" alt="joeleos" /></span><span class="nome">Joe & Leo's</span><span class="tempo">45 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="/assets/img/thumb_porcao.png" width="62" height="31" alt="porcão" /></span><span class="nome">Porcão Rio's</span><span class="tempo">55 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="/assets/img/thumb_outback.png" width="62" height="31" alt="outback" /></span><span class="nome">Outback Rio Sul</span><span class="tempo">0 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="/assets/img/thumb_gulagula.png" width="62" height="31" alt="gula gula" /></span><span class="nome">Gula Gula</span><span class="tempo">20 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="/assets/img/thumb_braz.png" width="62" height="31" alt="braz" /></span><span class="nome">Pizzaria Bráz</span><span class="tempo">40 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="/assets/img/thumb_joeleos.png" width="62" height="31" alt="joeleos" /></span><span class="nome">Joe & Leo's</span><span class="tempo">45 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_porcao.png" width="62" height="31" alt="porcão" /></span><span class="nome">Porcão Rio's</span><span class="tempo">55 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_outback.png" width="62" height="31" alt="outback" /></span><span class="nome">Outback Rio Sul</span><span class="tempo">0 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_gulagula.png" width="62" height="31" alt="gula gula" /></span><span class="nome">Gula Gula</span><span class="tempo">20 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_braz.png" width="62" height="31" alt="braz" /></span><span class="nome">Pizzaria Bráz</span><span class="tempo">40 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_joeleos.png" width="62" height="31" alt="joeleos" /></span><span class="nome">Joe & Leo's</span><span class="tempo">45 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_porcao.png" width="62" height="31" alt="porcão" /></span><span class="nome">Porcão Rio's</span><span class="tempo">55 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_outback.png" width="62" height="31" alt="outback" /></span><span class="nome">Outback Rio Sul</span><span class="tempo">0 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_gulagula.png" width="62" height="31" alt="gula gula" /></span><span class="nome">Gula Gula</span><span class="tempo">20 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_braz.png" width="62" height="31" alt="braz" /></span><span class="nome">Pizzaria Bráz</span><span class="tempo">40 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_joeleos.png" width="62" height="31" alt="joeleos" /></span><span class="nome">Joe & Leo's</span><span class="tempo">45 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-<li><span class="thumb"><img src="img/thumb_porcao.png" width="62" height="31" alt="porcão" /></span><span class="nome">Porcão Rio's</span><span class="tempo">55 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
-</ul>
- 
-</div>
-<div id="divider"><img src="img/divider.png" width="734" height="1" /></div>
-<div id="destaques"><a href="#"><img src="img/box_downloadapp_home.jpg" width="237" height="109" alt="download app" /></a> <a href="cadastro_rest.html"><img src="img/box_cadastrorest_home.jpg" width="237" height="109" alt="cadastro restaurante" hspace="5" /></a> <a href="reserva.html"><img src="img/box_reserva_home.jpg" width="237" height="109" alt="reserva mesa" hspace="5" /></a></div>
+<div class="container_24">
+    <div class="grid_24">
+	
+        <div class="grid_19 alpha">
+
+			<h1>Restaurantes:</h1>
+
+			<div id="alerta">*Tempo de espera na fila baseado em mesa para 4 pessoas</div>
+
+			<div id="gestao">
+
+				<div id="busca"><strong>Escreva o nome ou o bairro do resturante aqui: </strong><input name="" type="text" class="field3" /><a href="#">VEM</a> </div>
+			 
+				<span id="nome">Restaurante</span><span id="tempo">Tempo de fila</span><span id="mesas">N° de Mesas</span><span id="endereco">Endereço</span>
+
+				<div class="clear"></div>
+
+				<table>
+					<colgroup>
+						<col width="10%">
+						<col width="20%">
+						<col width="15%">
+						<col width="15%">
+						<col width="20%">
+						<col width="10%">
+					</colgroup>
+					<tr>
+						<th class="clean">&nbsp;</th>
+						<th>Restaurante</th>
+						<th>Tempo da fila</th>
+						<th>N° de Mesas</th>
+						<th>Endereço</th>
+						<th class="clean">&nbsp;</th>
+					</tr>
+					<tr>
+						<td><img src="/assets/img/thumb_outback.png" width="62" height="31" alt="outback" /></td>
+						<td>Outback Rio Sul</td>
+						<td>0 minutos</td>
+						<td>10 mesas</td>
+						<td>Rua Lauro Muller, 500</td>
+						<td><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></td>
+					</tr>
+				</table>
+
+				<div class="clear"></div>
+
+				<ul>
+					<li><span class="thumb"><img src="/assets/img/thumb_outback.png" width="62" height="31" alt="outback" /></span><span class="nome">Outback Rio Sul</span><span class="tempo">0 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+					<li><span class="thumb"><img src="/assets/img/thumb_gulagula.png" width="62" height="31" alt="gula gula" /></span><span class="nome">Gula Gula</span><span class="tempo">20 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+					<li><span class="thumb"><img src="/assets/img/thumb_braz.png" width="62" height="31" alt="braz" /></span><span class="nome">Pizzaria Bráz</span><span class="tempo">40 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+					<li><span class="thumb"><img src="/assets/img/thumb_joeleos.png" width="62" height="31" alt="joeleos" /></span><span class="nome">Joe & Leo's</span><span class="tempo">45 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+					<li><span class="thumb"><img src="/assets/img/thumb_porcao.png" width="62" height="31" alt="porcão" /></span><span class="nome">Porcão Rio's</span><span class="tempo">55 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+					<li><span class="thumb"><img src="/assets/img/thumb_outback.png" width="62" height="31" alt="outback" /></span><span class="nome">Outback Rio Sul</span><span class="tempo">0 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+					<li><span class="thumb"><img src="/assets/img/thumb_gulagula.png" width="62" height="31" alt="gula gula" /></span><span class="nome">Gula Gula</span><span class="tempo">20 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+					<li><span class="thumb"><img src="/assets/img/thumb_braz.png" width="62" height="31" alt="braz" /></span><span class="nome">Pizzaria Bráz</span><span class="tempo">40 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+					<li><span class="thumb"><img src="/assets/img/thumb_joeleos.png" width="62" height="31" alt="joeleos" /></span><span class="nome">Joe & Leo's</span><span class="tempo">45 minutos</span><span class="mesas">10 mesas</span><span class="endereco">Rua Lauro Muller, 500</span><span class="checkin"><a href="checkin.html"><img src="/assets/img/bt_checkin.png" width="102" height="26" alt="checkin" /></a></span></li>
+				</ul>
+			 
+			</div>
+
+            <div id="destaques"><a href="cadastro_lead.html"><img src="/assets/img/banner_home.jpg" width="737" height="200" alt="vem!" title="vem!" /></a></div>
+            
+            <div id="download_app"><a href="#"><img src="/assets/img/banner_app_home.png" width="737" height="77" alt="baixe aqui nosso app!" title="baixe aqui nosso app!" /></a></div>
+
+		</div>
+
+        <div class="grid_4 prefix_1 omega">
+            <a href="#"><img src="/assets/img/pub01.jpg" width="173" height="292" alt="pub01" /></a><br />
+            <a href="#"><img src="/assets/img/pub02.jpg" width="173" height="260" alt="pub02" vspace="5" /></a><br />
+            <a href="#"><img src="/assets/img/pub03.jpg" width="173" height="171" alt="pub03" vspace="5" /></a><br />
+        </div>
+
+	</div>
 </div>
 
-<div id="publicidade">
-  <a href="#"><img src="img/pub01.jpg" width="173" height="292" alt="pub01" /></a><br />
-  <a href="#"><img src="img/pub02.jpg" width="173" height="260" alt="pub02" vspace="5" /></a>
-</div>
-
-</div>
 <?=$footer?>
