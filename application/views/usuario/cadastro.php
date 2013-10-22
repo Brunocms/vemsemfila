649ddca4883f8002337e04db59a0fd4c1c24ef49
Frisone <frisonemx@gmail.com>
2013-10-21 23:13:33 -0200

    adicionado includes de publicidade no cadastro usuario

--- application/views/usuario/cadastro.php
+++ application/views/usuario/cadastro.php
@@ -89,6 +89,14 @@
           </fieldset>
           </form>
 
+          <div id="download_app"><a href="#"><img src="/assets/img/banner_app_home.png" width="737" height="77" alt="baixe aqui nosso app!" title="baixe aqui nosso app!" /></a></div>
+
+        </div>
+
+        <div class="grid_4 prefix_1 omega">
+            <a href="#"><img src="/assets/img/pub01.jpg" width="173" height="292" alt="pub01" /></a><br />
+            <a href="#"><img src="/assets/img/pub02.jpg" width="173" height="260" alt="pub02" vspace="5" /></a><br />
+            <a href="#"><img src="/assets/img/pub03.jpg" width="173" height="171" alt="pub03" vspace="5" /></a><br />
         </div>
 
     </div>
