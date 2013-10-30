3da6d796dd9f72c468dc552bbad420a6856e0473
caioreis <morettieq@gmail.com>
2013-10-30 21:10:47 -0200

    Alterando topo

---
+++ application/views/usuario/registro_sucesso.php
@@ -0,0 +1,19 @@
+<?=$header?>
+<?=$topo?>
+
+<div class="container_24" id="cadastro-usuario">
+    <div class="grid_24">
+  
+        <div class="grid_19 alpha">
+
+            <h1 style="font-weight: initial;font-size: 15px;">Cadastro efetivado com sucesso</h1>
+
+
+          <div id="download_app"><a href="#"><img src="/assets/img/banner_app_home.png" width="737" height="77" alt="baixe aqui nosso app!" title="baixe aqui nosso app!" /></a></div>
+
+        </div>
+
+    </div>
+</div>
+
+<?=$footer?>
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,8 +4,8 @@ set :repository,  "git@github.com:caioreis/vemsemfila.git"
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "ec2-107-22-73-129.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
-role :app, "ec2-107-22-73-129.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
+role :web, "ec2-107-21-195-199.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
+role :app, "ec2-107-21-195-199.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
