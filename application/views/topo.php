b42702339bafca9c1698336c779119c49d1e9f05
Frisone <frisonemx@gmail.com>
2013-11-25 23:38:21 -0200

    voltando a versao do menu e header antigo

--- application/views/topo.php
+++ application/views/topo.php
@@ -3,80 +3,67 @@
 <div id="fb-root"></div>
 
 <div id="boxes">
-    <!-- Janela Modal com Bloco de Nota -->
-    <div id="dialog2" class="window popin-login">
-        <img src="/assets/img/logo_login.png" width="81" height="67" />
-        <form action="<?=base_url('home/login')?>" method="POST">
-            <span class="login">LOGIN</span><br /><input name="email" type="text" /><br /><br />
-            <span class="login">SENHA</span><br /><input name="senha" type="password" />
-            <br>
-            <a href="#">Esqueceu sua senha, clique aqui</a><br><br>
-            <button type="submit">Vem!</button>
-        </form>
-        <a href="<?=$login_fb?>" class="facebook"><span class="fa fa-facebook-square"></span> Facebook </a> &nbsp;&nbsp;&nbsp;
-   <!--     <a href="#" class="googleplus"><span class="fa fa-google-plus-square"></span> Google+ </a> -->
-        <br><br>
-        <p>AINDA NÃO É CADASTRADO? <a href="<?=base_url('cadastro')?>">CLIQUE AQUI</a></p>
-    </div>
 
-    <div id="popin-esqueceu-senha" class="window">
-        <img src="/assets/img/logo_login.png" width="161" height="135" />
-    
+    <!-- Janela Modal com Bloco de Nota -->
+    <div id="dialog2" class="window">
+        <span class="logo_login">
+            <img src="/assets/img/logo_login.png" width="161" height="135" />
+        </span><br /><br />
+        <span class="pontilhado">.............................................................................................</span>
+        <br /><br />
         <form action="<?=base_url('home/login')?>" method="POST">
-            <span>DIGITE SEU EMAIL ABAIXO PARA RECUPERAR SUA SENHA:</span><br /><input name="email" type="text" /><br /><br />
-            <a href="#" class="bt">Enviar!</a>
+            <span class="login">LOGIN</span><br /><input name="email" type="text" class="field6" /><br /><br />
+            <span class="login">SENHA</span><br /><input name="senha" type="text" class="field7" /><span class="login2"> <a href="#">(Esqueceu?)</a></span><br /><br />
+            <span class="bt_vem_login">
+            <input type="submit" value="Vem!" style="background:url(/assets/img/bt_vem_login.png) no-repeat; width: 105; height: 47;" />
+            </span><br /><br />
+            <span class="pontilhado">.............................................................................................</span><br />
+            <span class="cadastro2">AINDA NÃO É CADASTRADO? <a href="<?=base_url('cadastro')?>">CLIQUE AQUI</a></span>
         </form>
-        <span>Email enviado para pacca@vemsemfila.com.br para recuperar a senha</span>
     </div>
     <!-- Fim Janela Modal com Bloco de Nota -->
     <!-- M�scara para cobrir a tela -->
     <div id="mask"></div>
 </div>
 
-<div id="header" class="interno">
+<div id="header">
     <div id="header_top">
         <div class="container_24">
-            <div id="logo"><a href="<?=base_url('home')?>"><img src="/assets/img/logo.png" width="154" height="136" alt="logo" /></a></div>
+            <div class="grid_24">
+                <div id="logo"><a href="<?=base_url('home')?>"><img src="/assets/img/logo.png" width="154" height="136" alt="logo" /></a></div>
+                <div class="grid_8 prefix_5 alpha">
+                    <a href="#dialog2" name="modal">Faça seu Login aqui!</a>
+                </div>
+                <div class="grid_11 omega">
+                    <p><a href="https://www.facebook.com/vemsemfila" target="_blank"><img src="/assets/img/icon_facebook.png" width="65" height="20" alt="facebook" /></a></p>
+                </div>
+            </div>
         </div>
     </div>
-    
     <div id="menu">
         <div class="container_24">
             <ul class="grid_24">
-                <li class="grid_4 prefix_5 alpha"><a href="<?=base_url('como_funciona')?>">Como funciona</a></li>
-                <li class="grid_6 alpha"><a href="<?=base_url('restaurantes')?>">Enconte um restaurante <span class="fa fa-search"></span></a></li>
-                <?php
-                    if(!$this->user->validate_session()) :
-                ?>
-                    <li class="grid_6 alpha">
-                        <div id="welcome"><a href="#dialog2" name="modal"><span class="fa fa-user"></span> Login</a> | <a class="bt" href="<?=base_url('cadastro')?>">Cadastre-se</a></div>
-                    </li>
-                <?php
-                    else:
-                ?>
-                    <li class="grid_3 alpha" style="width: 130px"><a href="<?=base_url('cadastro')?>"><span class="fa fa-user"></span> <?=$this->user->user_data->nome?></a></li>
-                    <li class="grid_3 alpha"><a href="<?=base_url('usuario/logout')?>">Sair <span class="fa fa-sign-out"></span></a></li>
-                <?php
-                    endif;
-                ?>
+                <li class="grid_1 prefix_5 alpha"><a href="<?=base_url('home')?>">Home</a></li>
+                <li class="grid_4"><a href="<?=base_url('quem_somos')?>">Quem somos</a></li>
+                <li class="grid_3"><a href="<?=base_url('como_funciona')?>">Como funciona</a></li>
+                <li class="grid_4"><a href="<?=base_url('restaurantes')?>">Restaurantes</a></li>
+                <li class="grid_1"><a href="<?=base_url('faq')?>">FAQ</a></li>
+                <li class="grid_3"><a href="<?=base_url('cadastro')?>">Cadastre-se</a></li>
+                <li class="grid_3 omega" style="text-align: right;"><a href="<?=base_url('fale_conosco')?>">Fale Conosco</a></li>
             </ul>
         </div>
     </div>
-
 </div>
 
-
 <!-- NAO ESQUECER DE VOLTAR O SLIDESHOW PARA A HOME E OCULTAR QUANDO FOR NAS INTERNAS -->
-    
-<!-- <div id="slideshow">
+<div id="slideshow">
     <div class="slider-wrapper theme-default">
         <div class="ribbon"></div>
         <div id="slider" class="nivoSlider">
+              <a href="<?=base_url('restaurantes')?>"><img src="/assets/img/slide01.jpg" width="1280" height="269" alt="" border="0" /></a> 
+              <!-- <a href="http://www.apple.com" target="_blank"><img src="/assets/img/slide02.jpg" width="1280" height="269" alt="" border="0" /></a>  -->
         </div>
     </div>
 </div>
- -->    
-
-
 
 
