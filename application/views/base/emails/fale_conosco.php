542d67462a8b8797e8ddb96a2b5a43f94dfdd7cb
caioreis <morettieq@gmail.com>
2013-12-07 08:47:33 -0200

    Finalizando fale conosco

---
+++ application/views/base/emails/fale_conosco.php
@@ -0,0 +1,13 @@
+Nome: <?=(isset($nome)) ? $nome : null?> <br>
+
+Assunto: <?=(isset($assunto)) ? $assunto : null?> <br>
+
+Email: <?=(isset($email)) ? $email : null?> <br>
+
+Empresa: <?=(isset($empresa)) ? $empresa : null?> <br>
+
+Telefone: <?=(isset($telefone)) ?  $telefone : null?> <br>
+
+Celular: <?=(isset($celular)) ? $celular: null?> <br>
+
+Mensagem: <?=(isset($mensagem)) ? $mensagem : null?> <br>
